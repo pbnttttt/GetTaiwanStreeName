15495f77fdcae5e46e8d0aeb64dadfd14a9b20e1
pbnttttt <pbnttttt@gmail.com>
2015-10-26 12:09:22 +0800

    修改輸出格式為json

--- GetTaiwanStreetName/Form1.cs
+++ GetTaiwanStreetName/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -54,7 +55,7 @@ namespace GetTaiwanStreetName
             ComboBox cb = sender as ComboBox;
             dataGridView2.DataSource = cityareaDT.Select("CityId=" + cb.SelectedValue.ToString()).CopyToDataTable();
             dataGridView2.Columns["CityId"].Visible = false;
-                //cityareaDT.AsEnumerable().Select(x => x.Field<string>("Cityarea")).ToList();
+            //cityareaDT.AsEnumerable().Select(x => x.Field<string>("Cityarea")).ToList();
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
@@ -82,63 +83,104 @@ namespace GetTaiwanStreetName
             dataGridView3.DataSource = source;
         }
 
+        //private async void button1_Click(object sender, EventArgs e)
+        //{
+        //    Stream myStream;
+        //    SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+        //    saveFileDialog1.Filter = "csv files (*.csv)|*.csv";
+        //    //saveFileDialog1.FilterIndex = 2;
+        //    saveFileDialog1.RestoreDirectory = true;
+        //    if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+        //    {
+        //        if ((myStream = saveFileDialog1.OpenFile()) != null)
+        //        {
+        //            button1.Enabled = false;
+        //            button1.Text += "(資料擷取中…)";
+
+        //            ConcurrentBag<OutputAddress> bag = new ConcurrentBag<OutputAddress>();
+        //            #region GetRemoteData
+        //            foreach (DataRow cityRow in cityDT.Rows)
+        //            {
+        //                string CityId = cityRow.Field<int>("Id").ToString();
+        //                DataRow[] cityareaRows = cityareaDT.Select("CityId=" + CityId);
+        //                int i = 1;
+        //                foreach (var cityareaRow in cityareaRows)
+        //                {
+        //                    string city = cityRow.Field<string>("City");
+        //                    string cityarea = cityareaRow.Field<string>("Cityarea");
+        //                    string result = await Utilities.GetAddress(city, cityarea);
+        //                    List<string> address = Utilities.ParserAddress(result);
+
+
+        //                    foreach (var addr in address)
+        //                    {
+        //                        OutputAddress oa = new OutputAddress
+        //                        {
+        //                            City = string.Format("{0:000}_{1}", Convert.ToInt16(CityId), city),
+        //                            CityArea = cityarea, //string.Format("{0:000}_{1}", i, cityarea),
+        //                            Address = addr
+        //                        };
+        //                        bag.Add(oa);
+        //                        i++;
+        //                    }
+        //                }
+        //            }
+        //            #endregion
+
+        //            StringBuilder sb = new StringBuilder();
+        //            foreach (var item in bag.Reverse())
+        //            {
+        //                sb.AppendFormat("{0},{1},{2}\r\n", item.City, item.CityArea, item.Address);
+        //            }
+        //            string output = sb.ToString();
+
+        //            await myStream.WriteAsync(Encoding.Unicode.GetBytes(output), 0, Encoding.Unicode.GetByteCount(output));
+        //            myStream.Close();
+
+        //            button1.Enabled = true;
+        //            button1.Text = "輸出資料";
+        //        }
+        //    }
+        //}
+
         private async void button1_Click(object sender, EventArgs e)
         {
-            Stream myStream;
-            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
-            saveFileDialog1.Filter = "csv files (*.csv)|*.csv";
-            //saveFileDialog1.FilterIndex = 2;
-            saveFileDialog1.RestoreDirectory = true;
-            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
             {
-                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                var path = fbd.SelectedPath;
+
+                button1.Enabled = false;
+                button1.Text += "(資料擷取中…)";
+
+                List<CityModel> bag = new List<CityModel>();
+                #region GetRemoteData
+                foreach (DataRow cityRow in cityDT.Rows)
                 {
-                    button1.Enabled = false;
-                    button1.Text += "(資料擷取中…)";
-
-                    ConcurrentBag<OutputAddress> bag = new ConcurrentBag<OutputAddress>();
-                    #region GetRemoteData
-                    foreach (DataRow cityRow in cityDT.Rows)
+                    CityModel city = new CityModel() { CityName = cityRow.Field<string>("City") };
+                    string CityId = cityRow.Field<int>("Id").ToString();
+                    DataRow[] cityareaRows = cityareaDT.Select("CityId=" + CityId);
+                    foreach (var cityareaRow in cityareaRows)
                     {
-                        string CityId = cityRow.Field<int>("Id").ToString();
-                        DataRow[] cityareaRows = cityareaDT.Select("CityId=" + CityId);
-                        int i = 1;
-                        foreach (var cityareaRow in cityareaRows)
+                        CityAreaModel area = new CityAreaModel { CityAreaName = cityareaRow.Field<string>("Cityarea") };
+                        string result = await Utilities.GetAddress(city.CityName, area.CityAreaName);
+                        List<string> address = Utilities.ParserAddress(result);
+                        foreach (var addr in address)
                         {
-                            string city = cityRow.Field<string>("City");
-                            string cityarea = cityareaRow.Field<string>("Cityarea");
-                            string result = await Utilities.GetAddress(city, cityarea);
-                            List<string> address = Utilities.ParserAddress(result);
-
-                            
-                            foreach (var addr in address)
-                            {
-                                OutputAddress oa = new OutputAddress
-                                {
-                                    City = string.Format("{0:000}_{1}", Convert.ToInt16(CityId), city),
-                                    CityArea = cityarea, //string.Format("{0:000}_{1}", i, cityarea),
-                                    Address = addr
-                                };
-                                bag.Add(oa);
-                                i++;
-                            }
+                            StreetModel street = new StreetModel { StreetName = addr };
+                            area.Street.Add(street);
                         }
+                        city.CityArea.Add(area);
                     }
-                    #endregion
-
-                    StringBuilder sb = new StringBuilder();
-                    foreach (var item in bag.Reverse())
-                    {
-                        sb.AppendFormat("{0},{1},{2}\r\n", item.City, item.CityArea, item.Address);
-                    }
-                    string output = sb.ToString();
-
-                    await myStream.WriteAsync(Encoding.Unicode.GetBytes(output), 0, Encoding.Unicode.GetByteCount(output));
-                    myStream.Close();
-
-                    button1.Enabled = true;
-                    button1.Text = "輸出資料";
+                    bag.Add(city);
                 }
+                #endregion
+                string output = JsonConvert.SerializeObject(bag);
+
+                System.IO.File.WriteAllText(System.IO.Path.Combine(path, "taiwan.json"), output, Encoding.UTF8);
+
+                button1.Enabled = true;
+                button1.Text = "輸出資料";
             }
         }
     }
---
+++ GetTaiwanStreetName/StreetModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetTaiwanStreetName
+{
+    public class CityModel
+    {
+        public string CityName { get; set; }
+        public List<CityAreaModel> CityArea { get; set; }
+
+        public CityModel()
+        {
+            CityArea = new List<CityAreaModel>();
+        }
+    }
+
+    public class CityAreaModel
+    {
+        public string CityAreaName { get; set; }
+        public List<StreetModel> Street { get; set; }
+
+        public CityAreaModel()
+        {
+            Street = new List<StreetModel>();
+        }
+    }
+
+    public class StreetModel
+    {
+        public string StreetName { get; set; }
+    }
+}
